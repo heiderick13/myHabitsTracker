1274a3dbdc3bf9929dedd5fb6bed9aea4f93f34b
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-01-26 18:58:11 -0300

    add: scrolling with mouse drag

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,9 @@ const form = document.querySelector("#form-habits");
 const days = document.querySelector(".days");
 const nlwSetup = new NLWSetup(form);
 
+let pressed = false;
+let startX = 0;
+
 const data = JSON.parse(localStorage.getItem("Habits")) || {};
 
 // functions
@@ -26,9 +29,32 @@ function saveForm() {
   localStorage.setItem("Habits", JSON.stringify(nlwSetup.data));
 }
 
+function mouseDownHandler(e) {
+  pressed = true;
+  startX = e.clientX;
+  this.style.cursor = "grabbing";
+}
+
+function mouseUpHandler(e) {
+  pressed = false;
+  startX = 0;
+  days.style.cursor = "grab";
+}
+
+function mouseMoveHandler(e) {
+  if (!pressed) {
+    return;
+  }
+
+  this.scrollLeft += startX - e.clientX;
+}
+
 // event handlers
 btnAddDay.addEventListener("click", newDay);
 form.addEventListener("change", saveForm);
+days.addEventListener("mousedown", mouseDownHandler);
+days.addEventListener("mousemove", mouseMoveHandler);
+window.addEventListener("mouseup", mouseUpHandler);
 
 // load page
 nlwSetup.setData(data);
